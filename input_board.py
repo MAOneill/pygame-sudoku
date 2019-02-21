a2ad228024e284b4f3f6b95d652ed3a042cc2887
Margaret O'Neill <margaret@oneillfish.com>
2019-02-21 11:57:51 -0500

    creating objects, loading array, and basic print

--- input_board.py
+++ input_board.py
@@ -15,6 +15,7 @@ class Cell():
         self.x_position = 0
         self.y_position = 0
         self.image = 'png'
+        self.value = value
 
 allcells = []
        
@@ -28,21 +29,31 @@ for eachrow in rawboard:
         cellname = "r%dc%d" % (row,col)
         # print (cellname)
         newcell = Cell(row,col,eachtuple[0],eachtuple[1]) #all named 'newcell'
+        exec('r%dc%d = newcell'% (row,col)) #create unique name for each object
         rowarray.append(newcell)   #append my object into the row array
     allcells.append(rowarray)       #append row array of cells into the big one
+#need to delete newcell
 
 print(len(allcells))  #should be 9
 print(allcells[3][5].answer) # <-- this works
 # print(allcells)
 print(newcell.answer)
+print("row5 col8 is %d" % r5c8.answer)
+print("row 9 col 6 initial value is %d" % r9c6.value)
 
-
-def print_grid2(cube,value):
+def print_grid(cube,what):
     #print first line:
     print("----"*9 + "-")      #top border  
     for i in range(9):
         print ("|",end='')  #first left border
         for each in cube[i]:
-            print ((" %s |" % (each[value],)), end='') #it is a string right now
+            if what == "answer":
+                print ((" %s |" % (each.answer,)), end='') #it is a string right now
+            else:
+                print ((" %s |" % (each.value,)), end='') #it is a string right now
+
         print (" ") #new line
         print("----"*9 + "-") #separator lines and bottom border
+
+print_grid(allcells,"value")
+print_grid(allcells,"answer")
---
+++ test3.py
@@ -0,0 +1,5 @@
+for j in range(1,10,1):
+    exec('var_%d = j'%j)
+
+print(var_2)
+
