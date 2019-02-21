5dd5f22ee9cf9028bf57b859607d306ae2d3dcaf
Margaret O'Neill <margaret@oneillfish.com>
2019-02-21 12:48:54 -0500

    changed object definition to be inside dictionary.  not working. wip

--- input_board.py
+++ input_board.py
@@ -9,25 +9,37 @@ class Cell():
         self.row = row
         self.col = col
         self.inner = 0  #compute thie
-        self.guess = None  #blank to start
-        self.answer = answer
         self.pencils = {}  #{} will object create this and set here
         self.x_position = 0
         self.y_position = 0
         self.image = 'png'
-        self.value = value
+
+        self.guess = None       #blank to start
+        self.value = value      #given in start cube
+        self.answer = answer    #if loaded in answers
+
+        self.name = "r%dc%d" % (row,col)
+
 
 class Known_cell(Cell):
-    pass
+    def __init__(self,row,col,value,answer):
+        super().__init__(row,col,value,answer)
+        self.answer = answer                    
+
 class Unknown_cell(Cell):
-    pass
+    def __init__(self,row,col,value,answer=None):
+        super().__init__(row,col,value,answer)
+        self.answer = answer
+        self.value = None
+
+
 
 #process to create all 81 objects AND load them into an array
 #I didn't not put this inside a function because I need the individual
 #objects accessable by their name
 
 allcells = []
-    
+cell = {}    
 row = 0
 for eachrow in rawboard:
     row += 1    #increment the row counter
@@ -35,19 +47,27 @@ for eachrow in rawboard:
     col = 0 
     for eachtuple in eachrow:
         col += 1
+        
+        #if the first value is KNOWN, then create a Known_cell class
+        #otherwise create an Unknown_cell class
+        if eachtuple[0] == 0:  #unknown
+            newcell = Unknown_cell(row,col,eachtuple[0],eachtuple[1]) #all named 'newcell'
+        else:
+            newcell = Known_cell(row,col,eachtuple[0],eachtuple[1]) #all named 'newcell'
         cellname = "r%dc%d" % (row,col)
-        # print (cellname)
-        newcell = Cell(row,col,eachtuple[0],eachtuple[1]) #all named 'newcell'
-        exec('r%dc%d = newcell'% (row,col)) #create unique name for each object
-        rowarray.append(newcell)   #append my object into the row array
+        cell['name'] = cellname
+        cell['data'] = newcell      #'data' dictionary key pair has the object
+
+        # exec('r%dc%d = newcell'% (row,col)) #create unique name for each object
+        # print(cell['data'].value)
+        rowarray.append(cell)   #append my object into the row array
+    print(rowarray)
     allcells.append(rowarray)       #append row array of cells into the big one
     #need to delete newcell at sometime
 
-print(len(allcells))  #should be 9
-print(allcells[3][5].answer) # <-- this works
+# print(len(allcells))  #should be 9
+# print(allcells[3][5].answer) # <-- this works
 # print(allcells)
-print("row5 col8 is %d" % r5c8.answer)
-print("row 9 col 6 initial value is %d" % r9c6.value)
 
 
 #function for printing my grid in python terminal
@@ -60,9 +80,9 @@ def print_grid(cube,what):
         print ("|",end='')  #first left border
         for each in cube[i]:
             if what == "answer":
-                print ((" %s |" % (each.answer,)), end='') #it is a string right now
+                print ((" %s |" % (each['data'].answer,)), end='') #it is a string right now
             else:
-                print ((" %s |" % (each.value,)), end='') #it is a string right now
+                print ((" %s |" % (each['data'].value,)), end='') #it is a string right now
 
         print (" ") #new line
         print("----"*9 + "-") #separator lines and bottom border
