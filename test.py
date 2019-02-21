e96716d94ef168262f67291d0f128eeda85c9f4f
Margaret O'Neill <margaret@oneillfish.com>
2019-02-21 08:48:26 -0500

    found out how to read tuple

---
+++ test.py
@@ -0,0 +1,11 @@
+tuple = (1,0)
+tuple2 = (4,7)
+tuple3 = (8,1)
+tuple_array = []
+tuple_array.append(tuple)
+tuple_array.append(tuple2)
+tuple_array.append(tuple3)
+
+print(tuple_array)
+print(tuple_array[0][1])  #is this the 2nd part of the first tuple, "0"
+print(tuple_array[2][0])  #thrid set, first element "8"
